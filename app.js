87ae4e1d8d60942998c3cac251a68be645c89574
Jiri Zezula <jiri.zezula@sap.com>
2019-03-06 15:25:43 +0100

    minimal working hook added to app.js
    minimal working construct added to app.js
    definition of app and controller added.
    
    handler for click event is still missing.

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-()();
+(
+function(){
+	'use strict';
+
+	angular.module("lunchCheckerApp", [])
+	.controller("lunchCheckerController", lunchCheckerController);
+
+	lunchCheckerController.$injector = ['$scope'];
+
+	function lunchCheckerController($scope){
+		console.log("controller");
+	};
+}
+
+)();
